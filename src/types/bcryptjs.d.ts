f08bc5e2bacb0e91eff6721c4db1b924af800f23
jialo1 <swompo@gmail.com>
2025-04-11 22:25:11 +0000

    Fix TypeScript errors and ESLint warnings

---
+++ src/types/bcryptjs.d.ts
@@ -0,0 +1,6 @@
+declare module 'bcryptjs' {
+  export function hashSync(data: string | Buffer, saltOrRounds: string | number): string;
+  export function compareSync(data: string | Buffer, encrypted: string): boolean;
+  export function hash(data: string | Buffer, saltOrRounds: string | number): Promise<string>;
+  export function compare(data: string | Buffer, encrypted: string): Promise<boolean>;
+} 
